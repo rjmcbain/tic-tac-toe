0fe0ce775fcfba7a58001aa1e5e3fe2bbb028537
rjmcbain <rmcbain01@gmail.com>
2017-12-08 15:00:11 -0700

    it works

--- js/app.js
+++ js/app.js
@@ -1,18 +1,100 @@
 console.log("OMG IM FUCKING ALIVE");
 
 
-var turn=0;
 
-$('.cell').on("click" ,function(){
-    
-    
-    if(turn % 2 === 0){
-        $(this).text('X');         
-    } else {
-       $(this).text('O');
+$(document).ready(function(){
+
+    var player = 1;
+
+    $('.square').on('click', function(event){
+
+        var squareSelected = $(this);
+
+        if(squareSelected.hasClass('ex') || squareSelected.hasClass('oh')){  
+
+            alert('Already Been Selected.');
+        } else {
+            if (player === 1) {
+                squareSelected.addClass('ex');
+                if(checkIfPlayerWon('ex')) {
+                    alert('Congrats! Player ' + player + ' has won!');
+                } else {
+                    player = 2;
+                }
+                
+            } else {
+                squareSelected.addClass('oh');
+                if(checkIfPlayerWon('oh')) {
+                    alert('Congrats! Player ' + player + ' has won!');
+                } else {
+                    player = 1;
+                }
+            }
+                // if (player === 1) {
+                // squareSelected.addClass('ex');
+                // if(checkIfPlayerWon('ex')) {
+                //     aler ('Congrats! Player ' + player + ' has won!');
+                // }
+                // player = 1;
+            
+        }
+    });
+
+    function checkIfPlayerWon(symbol) {
+
+           if($('.sq1').hasClass(symbol) && $('.sq2').hasClass(symbol) && $('.sq3').hasClass(symbol)) {
+            return true;
+         } else if ($('.sq4').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq6').hasClass(symbol)){
+                return true;
+         } else if ($('.sq7').hasClass(symbol) && $('.sq8').hasClass(symbol) && $('.sq9').hasClass(symbol)){
+                return true;
+         } else if($('.sq1').hasClass(symbol) && $('.sq4').hasClass(symbol) && $('.sq7').hasClass(symbol)) {
+            return true;
+         } else if ($('.sq2').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq8').hasClass(symbol)){
+                return true;
+         } else if ($('.sq3').hasClass(symbol) && $('.sq6').hasClass(symbol) && $('.sq9').hasClass(symbol)){
+                return true;
+         } else if ($('.sq1').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq9').hasClass(symbol)){
+                return true;
+         } else if ($('.sq3').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq7').hasClass(symbol)){
+                return true;
+         } else {
+            return false;
+         }
+
     }
-    turn++;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var turn=0;
+
+// $('.cell').on("click" ,function(){
+    
+    
+//     if(turn % 2 === 0){
+//         $(this).text('X');         
+//     } else {
+//        $(this).text('O');
+//     }
+//     turn++;
+// });
+
+
+
+
+
 
 
 
